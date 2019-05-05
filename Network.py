4253c468b957f552684717c58cb92f9d919059e4
Charlotte <xiangyinyu1127@gmail.com>
2019-05-05 17:25:43 -0400

    //

--- Network.py
+++ Network.py
@@ -74,8 +74,9 @@ class Network(object):
         
     #%% SGD to be applied to mini_batches
     def update_mini_batch(self, mini_batch, eta):
-        """Update the network's weights and biases by applying
-        gradient descent using backpropagation to a single mini batch.
+        """This updates the network's weights and biases by applying
+        gradient descent for the current mini_batch of training examples 
+        using backpropagation to a single mini batch.
         The "mini_batch" is a list of tuples (x, y)"""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
@@ -98,18 +99,17 @@ class Network(object):
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         
         # feedforward
-        activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
+        activation = x # Input: set correponding activation for input layer
+        activations = [x] # list to store all the activations
+        zs = [] # list to store all the z vectors
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
         
-        # backward pass
-        delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+        # BP1: output error δ
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
@@ -121,6 +121,7 @@ class Network(object):
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
+            # BP2: error in terms of the error in the next layer
             delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
@@ -139,6 +140,7 @@ class Network(object):
     def cost_derivative(self, output_activations, y):
         """Return the vector of partial derivatives \partial C_x /
         \partial a for the output activations."""
+        # BP1a: in the case of the quadratic cost function ▽aC = a^L-y
         return output_activations-y
 
         
